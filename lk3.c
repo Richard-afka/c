bc98441fc3bfa5c5c190e9c76d241d82f17d4afc
Richard-afka <2671530264@qq.com>
2025-10-29 20:34:54 +0800

    保存本地修改

--- lk3.c
+++ lk3.c
@@ -1,23 +1,29 @@
  #include<stdio.h>
  int main(){
-   int x;
-   scanf("%d",&x);
-   int n=0;
+ int n=0;
+ int x;
+ scanf("%d",&x);
     if(x<0){
+      if(x==-2147483648){
+        return 0;
+      }
        n=-x;
     }else{
        n=x;
     }
     int m=0;
-    int h=0;
+    long long h=0;
     while(n>0){
      m=n%10;
      n/=10;
      h=h*10+m;
     }
+  if(h>2147483647){
+    return 0;
+  }
   if(x<0){
-    printf("%d\n",-h);
+    printf("%lld\n",-h);
   }else{
-    printf("%d\n",h);
+    printf("%lld\n",h);
   }
 }
